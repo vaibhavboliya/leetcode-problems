204ca111a10d7295320b2c3041647b4e0d01e0cb
Vaibhav Boliya <53927146+vaibhavboliya@users.noreply.github.com>
2021-12-30 16:49:11 +0530

    Time: 101 ms (72.24%), Space: 65.1 MB (20.06%)

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -1,12 +1,15 @@
 class Solution {
     public int countPrimes(int n) {
+        if(n<=2) return 0;
+        
         boolean notPrime[] = new boolean[n];
-        int count = 0;
-        for(int i=2;i<n;i++){
+        int upper = (int) Math.sqrt(n);
+        int count = 1;
+        for(int i=3;i<n;i+=2){
             if(!notPrime[i]){
                 count++;
-                if(i*i<n)
-                    for(int j=2*i;j<n;j+=i){
+                if(i>upper) continue;
+                    for(int j=i*i;j<n;j+=i*2){
                         notPrime[j] = true;
                 }
             }

f206c9ed1ce9b82502735a5001db5c020efc8cf1
Vaibhav Boliya <53927146+vaibhavboliya@users.noreply.github.com>
2022-01-10 22:24:06 +0530

    Time: 1 ms (99.94%), Space: 45 MB (44.00%)

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 1,major = nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(count==0){
+                count++;
+                major = nums[i];
+            }else if(major==nums[i])
+                count++;
+            else
+                count--;
+        }
+        return major;
+        
+    }
+}

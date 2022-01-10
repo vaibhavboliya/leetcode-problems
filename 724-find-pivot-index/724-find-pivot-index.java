2bbd6db7468a9338770cdbe762641f6b7618b81e
Vaibhav Boliya <53927146+vaibhavboliya@users.noreply.github.com>
2022-01-10 22:16:30 +0530

    Time: 2 ms (43.92%), Space: 49.5 MB (11.06%)

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -4,9 +4,8 @@ class Solution {
         for(int i=0;i<nums.length;i++)
             totalSum +=nums[i];
         int leftSum =0;
-        for(int i=0;i<nums.length;i++){
-            leftSum +=nums[i];
-            if(leftSum*2==totalSum+nums[i]) 
+        for(int i=0;i<nums.length;leftSum+=nums[i++]){
+            if(leftSum*2==totalSum-nums[i]) 
                 return i;     
         }
         return -1;

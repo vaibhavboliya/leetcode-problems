ac771028e7a17e714c52101f6cd7f1aca6dfdec3
Vaibhav Boliya <53927146+vaibhavboliya@users.noreply.github.com>
2022-01-10 14:34:19 +0530

    Time: 1 ms (100.00%), Space: 40.4 MB (41.99%)

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,5 +1,6 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) return; 
         int count = 0;
         int idx = 0;
         for(int i = 0;i<nums.length;i++){
